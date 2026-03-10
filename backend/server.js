de3319f9d450091c11400d41f93f5d3525f82293
Guilherme <torres.guib2009@gmail.com>
2026-03-10 11:56:53 -0300

    primeiro commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"content-type": "text/html"})
+    res.end(`
+        
+    `)
+})
+
+server.listen(3000, () => {
+    console.log("servidor rodando em http://localhost:3000");
+})
