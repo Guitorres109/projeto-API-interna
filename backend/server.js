ba48b2ed0867d759ee4f3c6acb608e5f43ac9a0e
Guilherme <torres.guib2009@gmail.com>
2026-03-10 12:05:34 -0300

    primeiro commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
+//API de cachorros
+//feita com node.js + express
+
+//Agora as fotos NÃO são mais baixadas automaticamente
+//Elas devem existir manualmente na pasta:
+
+//data/fotos
+
 const http = require("http");
 
 const server = http.createServer((req, res) => {
